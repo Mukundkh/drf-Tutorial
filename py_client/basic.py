f2c657e61c5870e4721001092cbf537faf13c5f3
Mukund Khandelwal <pythondev@ongraph.com>
2022-04-02 12:17:02 +0530

    updated basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,11 @@ import requests
 endpoint = "http://httpbin.org/status/200"
 endpoint = "http://httpbin.org/anything"
 
-get_response = requests.get(endpoint) #HTTP Request
+get_response = requests.get(endpoint, data={"query" : "hello world"}) #HTTP Request
 print(get_response.text) #Print raw text response
 
+"""
+    HTTP REQUEST -> HTML,
+    WHEREAS REST API HTTP REQUEST -> JSON, XML
+"""
+print(get_response.json())
